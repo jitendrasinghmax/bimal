0fb2bbc34df8a6257382b78ff23b3d6bc9d195ec
jitendra <skybag83@gmail.com>
2024-11-17 23:05:37 +0530

    vercel.json  changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
+import Mycart from "./icon/mycart";
+
 const Navbar=()=>{
     return(<>
-        <div className="flex justify-between px-10 pt-5">
+        <div className="flex justify-between px-4 sm:px-10 pt-2 sm:pt-5">
             <div className="font-bold text-2xl text-orange-500">Bimal</div>
             <div className="flex gap-4 h-fi">
-                <div className="text-orange-950  mt-2 hidden sm:block">Home</div>
-                <div className="text-orange-950 mt-2 hidden sm:block">Contact</div>
-                <div className="text-orange-950 mt-2 hidden sm:block">About</div>
+                <button className="text-orange-950 mt-2 pb-2"><Mycart/></button>
                 <button className="bg-orange-500 text-sm sm:text-lg text-white px-2 rounded-md py-2">Die Now</button>
             </div>
         </div>
