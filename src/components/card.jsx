8d60ed6af2912e7e18503bc349f6ffdf0182a4a9
jitendra <skybag83@gmail.com>
2024-11-17 21:40:31 +0530

    responsive changes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,14 @@
 import Bimal from '../assets/image 3.png'
 const Card = () => {
     return (<>
-        <div className="w-full h-[202px] bg-orange-200 flex flex-col sm:flex-row justify-evenly sm:justify-between items-center gap-2 mx-auto px-8 rounded-2xl">
+        <div className="w-fit h-fit bg-orange-200 flex flex-col sm:flex-row justify-evenly sm:justify-between  gap-2 mx-auto px-8 py-3 rounded-2xl">
             <img src={Bimal} className='max-w-[168px]'></img>
-            <div className='flex flex-col gap-3 w-full'>
-                <div className='text-[20] font-medium text-orange-950 text-[10px] sm:text-sm text-center sm:text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
-                <div className='flex justify-evenly'>
-                    <div className='text-[20] font-extrabold text-orange-950 sm:mt-auto'>Rs. 99</div>
-                    <button className='p-2 bg-white rounded-md text-sm w-[93px] h-[36px] sm:w-[113px] sm:h-[56px] sm:text-lg sm:ml-auto'>Die Now</button>
+            <div className='flex flex-col gap-3 sm:gap-1 w-full'>
+                <div className='font-extrabold text-orange-950'>Bimal<span  className='font-extrabold text-orange-400'> Elachi</span></div>
+                <div className='text-[20] font-extrabold text-orange-950 sm:mt-auto'>Rs. 99</div>
+                <div className='flex gap-x-6 xm:ml-3'>
+                    <button className='p-2 bg-white rounded-md text-sm w-[93px] h-[36px] sm:w-[113px] sm:h-[56px] sm:text-lg '>Die Now</button>
+                    <button className='p-2 bg-orange-500 text-white rounded-md text-sm w-[93px] h-[36px] sm:w-[113px] sm:h-[56px] sm:text-lg '>Die Later</button>
                 </div>
             </div>
         </div>
