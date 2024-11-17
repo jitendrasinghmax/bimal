b6d263b6b4d0c78a7cddedcc68c2c7c7f06f8d21
jitendra <skybag83@gmail.com>
2024-11-17 21:53:30 +0530

    vercel.json  changes

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -2,7 +2,7 @@ import Card from "./card";
 
 const Content = () => {
     return (<>
-        <div className="max-w-[550px] sm:max-w-[600px] md:max-w-[900px] h-fit bg-orange-100 rounded-2xl mx-auto px-8 flex flex-col gap-10 pt-10">
+        <div className=" max-w-[550px] sm:max-w-[600px] md:max-w-[900px] h-fit bg-orange-100 rounded-2xl mx-auto px-8 flex flex-col gap-10 pt-10">
             <Card/>
             <Card/>
             <Card/>
