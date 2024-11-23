5be17635057380388befb2490c768e0e192fcad1
jitendra <skybag83@gmail.com>
2024-11-23 17:52:55 +0530

    fotter updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,11 @@
 const Footer=()=>{
     return(<>
-            <div className="items-center bg-orange-900">
+            <div className="items-center">
                     <div >
-                        <p className="text-center">Developed by @jitendra singh</p>
+                        <div className="flex justify-center">  
+                        <p className="text-center">Developed by</p>
+                        <p className="text-white font-bold">@jitendra singh</p>
+                        </div>
                         <p className="text-center">js8322870@gmail.com</p>
                     </div>
             </div>
